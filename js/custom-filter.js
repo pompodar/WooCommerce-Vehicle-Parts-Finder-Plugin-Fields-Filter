febf4115c1bf89f08269083241747bd1888ecd0a
pompodar <pompodar@gmail.com>
2023-09-09 13:42:24 +0300

    add cookies functionality

--- js/custom-filter.js
+++ js/custom-filter.js
@@ -1,13 +1,25 @@
 jQuery(document).ready(function ($) {
+    // Access the AJAX URL from the localized variable
+    var ajaxurl = custom_filter_vars.ajaxurl;
+
+    // Function to set a cookie with the selected values
+    function setCookie(name, value) {
+        document.cookie = name + "=" + value + ";path=/";
+    }
+
+    // Function to get a cookie value
+    function getCookie(name) {
+        var match = document.cookie.match(new RegExp(name + "=([^;]+)"));
+        return match ? match[1] : null;
+    }
+
     // Function to load options for the dependent filters
     function loadOptions(filter, parentValue) {
-        var ajaxurl = custom_filter_vars.ajaxurl;
-
         $.ajax({
-            url: ajaxurl, // This is the WordPress AJAX URL
+            url: ajaxurl,
             type: "POST",
             data: {
-                action: "custom_filter_ajax_handler", // The AJAX action you defined in your plugin
+                action: "custom_filter_ajax_handler",
                 filter: filter,
                 parent: parentValue,
             },
@@ -18,33 +30,74 @@ jQuery(document).ready(function ($) {
         });
     }
 
-    // When the 'Make' filter changes, load 'Model' options
-    $("#make-filter").on("change", function () {
-        var makeValue = $(this).val();
-        if (makeValue) {
-            loadOptions("model", makeValue);
-        } else {
-            $("#model-filter").html(
-                '<option value="">Select Make First</option>'
-            );
-            $("#model-filter").prop("disabled", true);
-        }
-    });
+    // When the page loads, populate the form fields with saved values
+    $("#make-filter").val(getCookie("make"));
+    $("#model-filter").val(getCookie("model"));
+    $("#year-filter").val(getCookie("year"));
+    $("#category-filter").val(getCookie("category"));
+    $("#brand-filter").val(getCookie("brand"));
 
-    // Similarly, implement logic for other dependent filters (Year, Category, Brand)
+    // Enable dependent filters if the previous filter has a value
+    if (getCookie("make")) {
+        loadOptions("model", getCookie("make"));
+        $("#model-filter").prop("disabled", false);
+    }
+
+    if (getCookie("model")) {
+        loadOptions("year", getCookie("model"));
+        $("#year-filter").prop("disabled", false);
+    }
+
+    if (getCookie("year")) {
+        loadOptions("category", getCookie("year"));
+        $("#category-filter").prop("disabled", false);
+    }
+
+    if (getCookie("category")) {
+        loadOptions("brand", getCookie("category"));
+        $("#brand-filter").prop("disabled", false);
+    }
+
+    // When a filter changes, save the selection in a cookie
+    $("#make-filter, #model-filter, #year-filter, #category-filter").on(
+        "change",
+        function () {
+            var filterId = $(this).attr("id");
+            var filterValue = $(this).val();
+
+            // Save the selection in a cookie
+            setCookie(filterId, filterValue);
+
+            // Enable and load options for the next filter
+            var nextFilterId = "";
+            if (filterId === "make-filter") {
+                nextFilterId = "model-filter";
+            } else if (filterId === "model-filter") {
+                nextFilterId = "year-filter";
+            } else if (filterId === "year-filter") {
+                nextFilterId = "category-filter";
+            } else if (filterId === "category-filter") {
+                nextFilterId = "brand-filter";
+            }
+
+            if (nextFilterId) {
+                loadOptions(nextFilterId.replace("-filter", ""), filterValue);
+                $("#" + nextFilterId).prop("disabled", false);
+            }
+        }
+    );
 
     // Prevent the form from submitting normally
     $("#custom-filter-form").on("submit", function (e) {
         e.preventDefault();
         var formData = $(this).serialize();
-        var ajaxurl = custom_filter_vars.ajaxurl;
 
         // Make an AJAX request to filter products
         $.ajax({
-            url: ajaxurl, // This is the WordPress AJAX URL
+            url: ajaxurl,
             type: "POST",
             data: {
-                action: "custom_filter_ajax_handler", // The AJAX action you defined in your plugin
+                action: "custom_filter_ajax_handler",
                 formData: formData,
             },
             success: function (response) {
--- templates/filter-form.php
+++ templates/filter-form.php
@@ -2,34 +2,34 @@
 ?>
 <form id="custom-filter-form">
     <!-- Make Filter -->
-    <label for="make-filter">Make:</label>
     <select id="make-filter" name="make">
+        <option value="" disabled>Make</option>
         <option value="make1">Make 1</option>
         <option value="make2">Make 2</option>
         <!-- Add more options as needed -->
     </select>
 
     <!-- Model Filter (Initially Disabled) -->
-    <label for="model-filter">Model:</label>
     <select id="model-filter" name="model" disabled>
+        <option value="" disabled>Model</option>
         <!-- Options for models will be loaded dynamically using JavaScript -->
     </select>
 
     <!-- Year Filter (Initially Disabled) -->
-    <label for="year-filter">Year:</label>
     <select id="year-filter" name="year" disabled>
+        <option value="" disabled>Year</option>
         <!-- Options for years will be loaded dynamically using JavaScript -->
     </select>
 
     <!-- Category Filter (Initially Disabled) -->
-    <label for="category-filter">Category:</label>
     <select id="category-filter" name="category" disabled>
+        <option value="" disabled>Category</option>
         <!-- Options for categories will be loaded dynamically using JavaScript -->
     </select>
 
     <!-- Brand Filter (Initially Disabled) -->
-    <label for="brand-filter">Brand:</label>
     <select id="brand-filter" name="brand" disabled>
+        <option value="" disabled>Brand</option>
         <!-- Options for brands will be loaded dynamically using JavaScript -->
     </select>
 
